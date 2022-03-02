7b46f444e922f39bcb2b89acb0ba488fa652fee1
Roopa Jamadar <roopajamadar14@gmail.com>
2022-03-02 14:32:22 +0530

    Added: Sign Up Api called

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -11,7 +11,7 @@ export class SignupComponent implements OnInit {
 
   registerForm!: FormGroup;
   submitted = false;
- // public showPassword: boolean = false;
+  public showPassword: boolean = false;
 
 
   constructor(private formBuilder: FormBuilder,private userService: UserService) {
@@ -51,6 +51,9 @@ export class SignupComponent implements OnInit {
       console.log("form is not valid fill form correctly")
       return;
     }
+  }
+  public checkboxPassword(): void {
+    this.showPassword = !this.showPassword;
   }
    
 }
